6870de1885199f74697f7e55ea32b1a1322d47a9
RobertoNunziato <RobertoNunziato@10.25.183.136>
2016-03-24 12:40:31 +0100

    Aggiornamento

--- ProgettoBingSearch/src/main/java/it/uniroma3/agiw/ProgettoBingSearch/ReadingFromFile.java
+++ ProgettoBingSearch/src/main/java/it/uniroma3/agiw/ProgettoBingSearch/ReadingFromFile.java
@@ -8,7 +8,7 @@ public class ReadingFromFile {
 
 	public static void main(String[] args) {
 
-		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Daniela\\Documents\\test.txt")))
+		try (BufferedReader br = new BufferedReader(new FileReader("../ProgettoBingSearch/src/prova.txt")))
 		{
 
 			String sCurrentLine;
