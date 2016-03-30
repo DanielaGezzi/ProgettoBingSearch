e46319820c62b72b58c77f038a1715aefb27afe0
stefanotrinei <ste.trinei@stud.uniroma3.it>
2016-03-30 15:19:26 +0200

    patch problema stream

--- ProgettoBingSearch/src/main/java/it/uniroma3/agiw/ProgettoBingSearch/CreateFileQuery.java
+++ ProgettoBingSearch/src/main/java/it/uniroma3/agiw/ProgettoBingSearch/CreateFileQuery.java
@@ -15,6 +15,7 @@ public class CreateFileQuery {
 	
 	public void writeFileQuery(String query,JSONObject aQuery, JSONObject aResult) throws IOException {;
 		this.listaQuery.write(query+"\t"+aQuery.get("uri")+"\t"+aResult.get("Url")+"\n");
+	
 	}
 
 	public FileWriter getListaQuery() {
@@ -23,5 +24,10 @@ public class CreateFileQuery {
 
 	public void setListaQuery(FileWriter listaQuery) {
 		this.listaQuery = listaQuery;
+	}
+
+	public void close() throws IOException {
+		this.listaQuery.close();
+		
 	}
 }
