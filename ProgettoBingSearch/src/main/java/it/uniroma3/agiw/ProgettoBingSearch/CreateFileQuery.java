9f59d08f70c472b33afa839bc48416d080fd0f09
emanuele <emanuele@10.25.202.138>
2016-03-30 15:23:26 +0200

    boh

--- ProgettoBingSearch/src/main/java/it/uniroma3/agiw/ProgettoBingSearch/CreateFileQuery.java
+++ ProgettoBingSearch/src/main/java/it/uniroma3/agiw/ProgettoBingSearch/CreateFileQuery.java
@@ -8,13 +8,16 @@ import org.json.JSONObject;
 public class CreateFileQuery {
 	
 	private FileWriter listaQuery;
+	private FileWriter prova;
 	
 	public CreateFileQuery() throws IOException{
-		this.listaQuery = new FileWriter("../ProgettoBingSearch/src/listaQuery.txt");;
+		this.listaQuery = new FileWriter("../ProgettoBingSearch/src/listaQuery.txt");
+		this.prova = new FileWriter("../ProgettoBingSearch/src/prova.txt");
 	}
 	
 	public void writeFileQuery(String query,JSONObject aQuery, JSONObject aResult) throws IOException {;
 		this.listaQuery.write(query+"\t"+aQuery.get("uri")+"\t"+aResult.get("Url")+"\n");
+		this.prova.write(aResult.get("Url")+"\n");
 	}
 
 	public FileWriter getListaQuery() {
